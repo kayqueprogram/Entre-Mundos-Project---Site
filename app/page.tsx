56faa99135a8205fb11444c2e0394550cb6f1a4c
kayqueprogram <kayquejesusdn@gmail.com>
2024-09-08 17:41:48 -0300

    teste

--- app/page.tsx
+++ app/page.tsx
@@ -15,8 +15,6 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Entre Mundos - Visual Novel(Comtemplado na LPG)",
   description: "Projeto de game Visual Novel, contemplado no edital de Conteúdos Digitais, da Lei Paulo Gustavo, de incentivo á produção cultural - 2024. (Lei Complementar n.º 195, de 08 de julho de 2022)",
-  src: "https://cdn.discordapp.com/attachments/1217656113546268754/1282439381453377557/image1.jpg?ex=66df5c3f&is=66de0abf&hm=7eef3aef386788904055748fe767858f6d0ee2286e42dc08590e2d76706bc36d&",
-  // outros metadados
 };
 
 export default function Home() {
